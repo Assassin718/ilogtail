2f37c8ab54c27aaf45c501c1650e11596c6e21dc
github-action-benchmark <github@users.noreply.github.com>
2024-07-20 05:41:05 +0000

    add benchmark (customSmallerIsBetter) benchmark result for 4f3a5edc685ec5e51e375214f6fed9c1c4e8f26e

---
+++ dev/bench/data.js
@@ -0,0 +1,112 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1721454065203,
+  "repoUrl": "https://github.com/Assassin718/ilogtail",
+  "entries": {
+    "benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "13038089398@163.com",
+            "name": "Assassin718",
+            "username": "Assassin718"
+          },
+          "committer": {
+            "email": "13038089398@163.com",
+            "name": "Assassin718",
+            "username": "Assassin718"
+          },
+          "distinct": true,
+          "id": "4f3a5edc685ec5e51e375214f6fed9c1c4e8f26e",
+          "message": "update benchmark CI file",
+          "timestamp": "2024-07-20T13:15:05+08:00",
+          "tree_id": "ee6f161f91ebbcc0da975c332f8015743849b815",
+          "url": "https://github.com/Assassin718/ilogtail/commit/4f3a5edc685ec5e51e375214f6fed9c1c4e8f26e"
+        },
+        "date": 1721454063789,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CPU_Usage_Max-performance_file_to_blackhole_fluentbit",
+            "value": 7.798648548855355,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg-performance_file_to_blackhole_fluentbit",
+            "value": 7.382101533644073,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max-performance_file_to_blackhole_fluentbit",
+            "value": 20.5859375,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg-performance_file_to_blackhole_fluentbit",
+            "value": 20.36009457236842,
+            "unit": "MB"
+          },
+          {
+            "name": "CPU_Usage_Max-performance_file_to_blackhole_vector",
+            "value": 7.25420812845264,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg-performance_file_to_blackhole_vector",
+            "value": 6.803120571850048,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max-performance_file_to_blackhole_vector",
+            "value": 48.2890625,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg-performance_file_to_blackhole_vector",
+            "value": 47.76133765243904,
+            "unit": "MB"
+          },
+          {
+            "name": "CPU_Usage_Max-performance_file_to_blackhole_filebeat",
+            "value": 30.878699611530774,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg-performance_file_to_blackhole_filebeat",
+            "value": 3.71157652602222,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max-performance_file_to_blackhole_filebeat",
+            "value": 63.359375,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg-performance_file_to_blackhole_filebeat",
+            "value": 59.84203506097562,
+            "unit": "MB"
+          },
+          {
+            "name": "CPU_Usage_Max-performance_file_to_blackhole_ilogtail",
+            "value": 15.8297917886699,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg-performance_file_to_blackhole_ilogtail",
+            "value": 1.20690984275144,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max-performance_file_to_blackhole_ilogtail",
+            "value": 61.77734375,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg-performance_file_to_blackhole_ilogtail",
+            "value": 58.95998475609755,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
