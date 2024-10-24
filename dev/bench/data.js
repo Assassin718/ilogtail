f589f9342cd4ab6e30d95317c7336db1aedef200
github-action-benchmark <github@users.noreply.github.com>
2024-10-24 09:17:35 +0000

    add benchmark (customSmallerIsBetter) benchmark result for 4f41d948423f8fce8cd810c1adc701695509d761

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729759556704,
+  "lastUpdate": 1729761455087,
   "repoUrl": "https://github.com/Assassin718/ilogtail",
   "entries": {
     "benchmark": [
@@ -43,6 +43,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Memory_Usage_Avg - performance_file_to_blackhole_ilogtail",
             "value": 33.492431640624986,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "13038089398@163.com",
+            "name": "QianyongXu",
+            "username": "Assassin718"
+          },
+          "committer": {
+            "email": "13038089398@163.com",
+            "name": "QianyongXu",
+            "username": "Assassin718"
+          },
+          "distinct": true,
+          "id": "4f41d948423f8fce8cd810c1adc701695509d761",
+          "message": "update monitor stop logic",
+          "timestamp": "2024-10-24T01:50:44-07:00",
+          "tree_id": "83dd14ee3b8b882df615744b2f3b99e2878be859",
+          "url": "https://github.com/Assassin718/ilogtail/commit/4f41d948423f8fce8cd810c1adc701695509d761"
+        },
+        "date": 1729761454476,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CPU_Usage_Max - performance_file_to_blackhole_ilogtail",
+            "value": 40.85064514253977,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg - performance_file_to_blackhole_ilogtail",
+            "value": 24.40952928741572,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max - performance_file_to_blackhole_ilogtail",
+            "value": 33.921875,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg - performance_file_to_blackhole_ilogtail",
+            "value": 33.38779105392158,
             "unit": "MB"
           }
         ]
